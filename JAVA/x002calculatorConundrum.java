0ef66ba8796f424f5cd21c0b3814008cd18b9edc
joaquinmezzano <mezzanojoaquin@gmail.com>
2024-12-21 17:14:25 -0300

    README updated and (JAVA) exercise 2 updated

--- JAVA/x002calculatorConundrum.java
+++ JAVA/x002calculatorConundrum.java
@@ -36,20 +36,21 @@ To make matters simple, methods for calculating addition, multiplication and div
 The goal is to have a working calculator that returns a String with the following pattern: 16 + 51 = 67, 
 when provided with arguments 16, 51 and +.
 
-The main method for implementation in this task will be the CalculatorConundrum.calculate() method. It takes three arguments. 
+1. The main method for implementation in this task will be the CalculatorConundrum.calculate() method. It takes three arguments. 
 The first two arguments operand1 and operand2 are integer numbers on which an operation is going to operate. 
-The third argument operation is of type String and for this exercise it is necessary to implement the following operations:
+
+2. The third argument operation is of type String and for this exercise it is necessary to implement the following operations:
     addition using the + String
     multiplication using the * String
     division using the / String
 
-Update the CalculatorConundrum.calculate() method to handle illegal operations:
+3. Update the CalculatorConundrum.calculate() method to handle illegal operations:
     When the operation argument is null, an IllegalArgumentException should be thrown with the message Operation cannot be null.
     When the operation argument is "", an IllegalArgumentException should be thrown with the message Operation cannot be empty.
     When the operation argument is any operation other than +, *, or /, 
     an IllegalOperationException should be thrown with the message Operation '{operation}' does not exist.
 
-In Java, attempting to divide by zero throws an ArithmeticException. Update the CalculatorConundrum.calculate() method 
+4. In Java, attempting to divide by zero throws an ArithmeticException. Update the CalculatorConundrum.calculate() method 
 to catch this exception and then throw an IllegalOperationException with the message Division by zero is not allowed and 
 the caught ArithmeticException as its cause.
 */
--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 ### Páginas utilizadas
 - JAVA: https://exercism.org/tracks/java/
 - JAVASCRIPT: https://exercism.org/tracks/javascript/
-- SQL: https://www.freecodecamp.org/learn/relational-database/
+- SQL: https://www.codecademy.com/courses/learn-sql/
+       https://sqlbolt.com/
